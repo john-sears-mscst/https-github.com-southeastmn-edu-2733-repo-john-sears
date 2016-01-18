c31896747a2641160afa2a10497d7ff153e4e3e3
John Sears <sq8288kl@MY.SouthEastMN.Edu>
2016-01-18 11:44:02 -0600

    Commit Exercise 1A

---
+++ 2733jsears/jsears2733ex1a1/plants.js
@@ -0,0 +1,21 @@
+﻿/* JavaScript 6th Edition
+    
+    Chapter 1
+    
+    Chapter case
+    
+
+    Tinley Xeriscapes
+    Author: John Sears
+    Date: 01/18/2016
+
+
+    Filename: plants.js
+
+*/
+
+//define variables containing img src values
+
+var blanket = "images/blanket.jpg";
+var bluestem = "images/bluestem.jpg";
+var rugosa = "images/rugosa.jpg";
